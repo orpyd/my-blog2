9513bd3dfc43b95400837081e30998fce5b0acf4
Kioumars Ghereghlou <iCloud@Kioumarss-MacBook-Pro.local>
2020-05-09 18:23:45 -0400

    First commit

---
+++ src/build/precache-manifest.55f0ac3dc06ae0172a27f57a2d9cf339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56eeda90b41ad6c18c1a8ce661717fbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ffe9937c25ae498e6a2",
+    "url": "/static/css/main.7d8a4c31.chunk.css"
+  },
+  {
+    "revision": "e781da3a4160f68d7187",
+    "url": "/static/js/2.3ce0e83e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3ce0e83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ffe9937c25ae498e6a2",
+    "url": "/static/js/main.fae51c06.chunk.js"
+  },
+  {
+    "revision": "feaa447b41db17e49cf3",
+    "url": "/static/js/runtime-main.ab742bc3.js"
+  }
+]);
